ba4e414eeac8cf73cf7f6ece799edb55b8caf92b
leehaejoon <haejoon309@gmail.com>
2019-06-26 10:55:10 +0000

    [fix] directory structure

---
+++ src/01.string_reverse/min/min_reversestring.py
@@ -0,0 +1,12 @@
+file = open('sample.md', "r", encoding='UTF-8')
+f = open('result_min.md','w', encoding='UTF-8')
+for line in file:
+    num = line.count('#')
+    size=line[0:num]
+    line=line[2:]
+    reline=line[::-1]
+    output=size+' '+reline.strip('\n')
+    print(output)
+    f.write(output+'\n')
+file.close()
+f.close()
