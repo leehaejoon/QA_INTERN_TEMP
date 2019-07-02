06db1d53b8d141e2141a4c3623b8596a1d0344eb
ollehQ <vkdkf55@naver.com>
2019-07-02 09:13:11 +0900

    Add files via upload

--- src/go/change.py
+++ src/go/change.py
@@ -1,16 +1,18 @@
 import sys
 
 i=1
-line =''
+
+line = ''
+tmp = ''
 f= open('sample.md','r',encoding='UTF-8')
-while i <5:
-	line += f.readline()
-	i+=1
-line = line[::-1]
+for line in f:
+        line = line[::-1]
+        tmp += line
+        i+=1
+        print(line)
 f.close()
-print(line)
 d = open('result.md','w',encoding='UTF-8')
-d.write(line)
+d.write(tmp)
         
 d.close()
 
