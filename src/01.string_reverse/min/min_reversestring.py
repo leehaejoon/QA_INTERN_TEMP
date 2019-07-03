2fd213a85fbfd49d1cc69487a89b917967ffddea
이해준 <haejoon309@gmail.com>
2019-07-03 16:29:47 +0900

    코드 스타일 수정

--- src/01.string_reverse/min/min_reversestring.py
+++ src/01.string_reverse/min/min_reversestring.py
@@ -1,12 +1,12 @@
-file = open('sample.md', "r", encoding='UTF-8')
-f = open('result_min.md','w', encoding='UTF-8')
+file = open('sample.md', "r", encoding='UTF-8')  # 'file'의 경우 python2 에서는 내장함수이므로 변수로 사용을 지양
+f = open('result_min.md', 'w', encoding='UTF-8')
 for line in file:
     num = line.count('#')
-    size=line[0:num]
-    line=line[2:]
-    reline=line[::-1]
-    output=size+' '+reline.strip('\n')
+    size = line[0:num]
+    line = line[2:]
+    reline = line[::-1]
+    output = size + ' ' + reline.strip('\n')
     print(output)
-    f.write(output+'\n')
+    f.write(output + '\n')
 file.close()
 f.close()
