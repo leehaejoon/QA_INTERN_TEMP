08a5a1eea2226f5a99158e494239a3cdcf7aa9a0
GO <vkdkf55@naver.com>
2019-07-05 14:00:05 +0900

    [fix] 예외처리 하나 추가

--- src/03.database_basic/go/test_register_user_01.py
+++ src/03.database_basic/go/test_register_user_01.py
@@ -10,7 +10,11 @@ test_DB_file = 'test_user_info.db'
 
 class test_register_user_01(unittest.TestCase):
     def setUp(self):
-        conn = sqlite3.connect(test_DB_file)
+
+        if os.path.isfile(test_DB_file) == True:#파일이 이미 존재하는 경우를 위한 예외처리
+            conn = sqlite3.connect(test_DB_file+'2')
+        else :
+            conn = sqlite3.connect(test_DB_file)
         cur = conn.cursor()
         cur.execute('''CREATE table REGISTER_DATE (USER_ID,yesy,month,day,hour,min,second)''')
         conn.close()
